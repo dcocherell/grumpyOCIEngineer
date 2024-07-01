884b1285036582a9aab4df5425646f2bdc56b5a5
dcocherell <dcocherell@choctawnation.com>
2024-07-01 08:48:17 -0500

    Added delete post functionality.

--- grumpyociblog/theblog/forms.py
+++ grumpyociblog/theblog/forms.py
@@ -8,14 +8,20 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={
                 'class': 'form-control form-control-lg shadow',
-                'placeholder': 'Enter post title'
+                'placeholder': 'Enter post title',
+                'color': 'black'
             }),
             'author': forms.Select(attrs={
-                'class': 'form-select form-select-lg shadow'
+                'class': 'form-select form-select-lg shadow',
+                'color': 'black'
             }),
             'body': forms.Textarea(attrs={
                 'class': 'form-control shadow',
                 'rows': 5,
-                'placeholder': 'Write your post here'
+                'placeholder': 'Write your post here',
+                'color': 'black'
             }),
         }
+
+        # ensure that each widget field is visible on the background
+        
